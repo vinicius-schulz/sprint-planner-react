22974cfb71618cd4ae5badf2cc38f7d95327cec5
Vinicius Schulz <vinicius-schulz@hotmail.com>
2026-01-06 15:14:26 -0300

    feat: refactor routing structure by moving routes to AppRoutes component for better organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import { FollowUpPage } from './pages/FollowUpPage';
-import { PlanningPage } from './pages/PlanningPage';
-import { SprintListPage } from './pages/SprintListPage';
-import { Layout } from './layout/Layout';
-import { ProjectListPage } from './pages/ProjectListPage';
+import { AppRoutes } from './routes/AppRoutes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/projects" replace />} />
-          <Route path="projects" element={<ProjectListPage />} />
-          <Route path="projects/:projectId/sprints" element={<SprintListPage />} />
-          <Route path="plan" element={<Navigate to="/projects" replace />} />
-          <Route path="plan/:sprintId" element={<PlanningPage />} />
-          <Route path="acomp" element={<Navigate to="/projects" replace />} />
-          <Route path="acomp/:sprintId" element={<FollowUpPage />} />
-        </Route>
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Layout } from '../layout/Layout';
+import { ProjectListPage } from '../pages/ProjectListPage';
+import { SprintListPage } from '../pages/SprintListPage';
+import { PlanningPage } from '../pages/PlanningPage';
+import { FollowUpPage } from '../pages/FollowUpPage';
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="/projects" replace />} />
+        <Route path="projects" element={<ProjectListPage />} />
+        <Route path="projects/:projectId/sprints" element={<SprintListPage />} />
+        <Route path="plan" element={<Navigate to="/projects" replace />} />
+        <Route path="plan/:sprintId" element={<PlanningPage />} />
+        <Route path="acomp" element={<Navigate to="/projects" replace />} />
+        <Route path="acomp/:sprintId" element={<FollowUpPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default AppRoutes;
